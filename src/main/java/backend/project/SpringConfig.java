8040981637f3364d9522b87dcc37adbc8192cb0d
Minhyeok Jeon <minhyeogjeon945@gmail.com>
2023-10-17 21:18:37 +0900

    자바 코드로 직접 스프링 빈 등록하기

---
+++ src/main/java/backend/project/SpringConfig.java
@@ -0,0 +1,21 @@
+package backend.project;
+
+import backend.project.repository.MemberRepository;
+import backend.project.repository.MemoryMemberRepository;
+import backend.project.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}

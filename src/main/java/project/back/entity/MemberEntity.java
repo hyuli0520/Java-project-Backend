a59d7ce7b5605d78f645dd8fa58eda67cbfcf56f
Minhyeok Jeon <minhyeogjeon945@gmail.com>
2023-10-26 09:26:15 +0900

    회원가입 완료

--- src/main/java/project/back/entity/MemberEntity.java
+++ src/main/java/project/back/entity/MemberEntity.java
@@ -2,6 +2,7 @@ package project.back.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import project.back.dto.MemberDTO;
 
 import javax.persistence.*;
 
@@ -22,4 +23,12 @@ public class MemberEntity {
 
     @Column
     private String memberName;
+
+    public static MemberEntity toMemberEntity(MemberDTO memberDTO) {
+        MemberEntity memberEntity = new MemberEntity();
+        memberEntity.setMemberEmail(memberDTO.getMemberEmail());
+        memberEntity.setMemberPassword(memberDTO.getMemberPassword());
+        memberEntity.setMemberName(memberDTO.getMemberName());
+        return memberEntity;
+    }
 }
--- src/main/java/project/back/service/MemberService.java
+++ src/main/java/project/back/service/MemberService.java
@@ -1,6 +1,7 @@
 package project.back.service;
 
 import project.back.dto.MemberDTO;
+import project.back.entity.MemberEntity;
 import project.back.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,10 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     public void save(MemberDTO memberDTO) {
-
+        // 1. dto -> entity 변환
+        // 2. repository의 save 메서드 호출
+        MemberEntity memberEntity = MemberEntity.toMemberEntity(memberDTO);
+        memberRepository.save(memberEntity);
+        // repository의 save 메서드 호출 (조건. entity겍체를 넘겨줘야 함)
     }
 }

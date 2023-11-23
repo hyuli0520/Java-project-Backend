5c528936f67b71da82d8fdb77aa78798a51b6018
Minhyeok Jeon <minhyeogjeon945@gmail.com>
2023-11-23 15:38:54 +0900

    cloudtype

--- src/main/java/board/api/controller/MainController.java
+++ src/main/java/board/api/controller/MainController.java
@@ -1,9 +1,10 @@
 package board.api.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class MainController {
     // 기본 페이지 요청 메서드
